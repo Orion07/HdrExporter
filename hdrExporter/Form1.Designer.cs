a6d5c5aec69c5457544caa5b0f2dbb9850e4b76b
Orion07 <aligocmen7@gmail.com>
2016-07-27 14:17:09 +0300

    proje githuba yüklendi
    proje githuba yüklendi

---
+++ hdrExporter/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace hdrExporter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem3 = new System.Windows.Forms.ToolStripMenuItem();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripLabel1 = new System.Windows.Forms.ToolStripLabel();
+            this.menuStrip1.SuspendLayout();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(476, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem2,
+            this.toolStripMenuItem3});
+            this.toolStripMenuItem1.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(51, 20);
+            this.toolStripMenuItem1.Text = "Dosya";
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(155, 22);
+            this.toolStripMenuItem2.Text = "Yükle ve Cikart";
+            this.toolStripMenuItem2.Click += new System.EventHandler(this.toolStripMenuItem2_Click);
+            // 
+            // toolStripMenuItem3
+            // 
+            this.toolStripMenuItem3.Name = "toolStripMenuItem3";
+            this.toolStripMenuItem3.Size = new System.Drawing.Size(152, 22);
+            this.toolStripMenuItem3.Text = "Çıkış";
+            this.toolStripMenuItem3.Click += new System.EventHandler(this.toolStripMenuItem3_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Location = new System.Drawing.Point(0, 27);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(476, 275);
+            this.listView1.TabIndex = 1;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.toolStrip1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripLabel1});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 304);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(476, 25);
+            this.toolStrip1.TabIndex = 3;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripLabel1
+            // 
+            this.toolStripLabel1.Name = "toolStripLabel1";
+            this.toolStripLabel1.Size = new System.Drawing.Size(34, 22);
+            this.toolStripLabel1.Text = "Hazır";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(476, 329);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.menuStrip1);
+            this.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "HDR Exporter";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem3;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel1;
+    }
+}
+
---
+++ hdrExporter/Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace hdrExporter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public byte[] ReadAllBytes(string fileName)
+        {
+            byte[] buffer = null;
+            using (FileStream fs = new FileStream(fileName, FileMode.Open, FileAccess.Read))
+            {
+                buffer = new byte[fs.Length];
+                fs.Read(buffer, 0, (int)fs.Length);
+            }
+            return buffer;
+        }
+        public byte[] belliYeriOku(string dosyaAdi,int boyut,int offset)
+        {
+            byte[] tmp = null;
+            using(FileStream stream = new FileStream(dosyaAdi,FileMode.Open,FileAccess.Read))
+            {
+                stream.Seek(offset, SeekOrigin.Begin);
+                tmp = new byte[boyut];
+                stream.Read(tmp, 0, boyut);
+            }
+            return tmp;
+        }
+        public void olusturYaz(string yeniDosyaAdi,byte[] veri,int boyut)
+        {
+            using (FileStream fs =  File.Create(yeniDosyaAdi))
+            {
+                fs.Write(veri, 0, boyut);  
+            }
+        }
+        public void Cikart(string Klasor,string hedefSrc,string dosyaAdi,int adres,int boyut)
+        {
+            string[] parcala = hedefSrc.Split('.');
+            string srcAdres = Klasor + "\\" + parcala[0] + ".src";
+            byte[] okunan = belliYeriOku(srcAdres, boyut, adres);
+            string[] str = dosyaAdi.Split('\\');
+            string exporterKlasor = Klasor + "\\hdrExporter\\";
+            if (!Directory.Exists(exporterKlasor))
+            {
+                Directory.CreateDirectory(exporterKlasor);
+            }
+            if (str.Length == 1)
+            {
+                exporterKlasor += dosyaAdi;
+            }
+            else 
+            {
+                for (int i = 0; i < str.Length -1 ; i++)
+                {
+                    exporterKlasor += str[i] + "\\";
+                }
+                if (!Directory.Exists(exporterKlasor))
+                {
+                    Directory.CreateDirectory(exporterKlasor);
+                }
+                exporterKlasor += str[str.Length - 1];
+            }
+
+            //Console.WriteLine(Klasor + "\\hdrExporter\\" + dosyaAdi);
+            olusturYaz(exporterKlasor, okunan, boyut);
+        }
+        private void toolStripMenuItem3_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void toolStripMenuItem2_Click(object sender, EventArgs e)
+        {
+            listView1.Items.Clear();
+            toolStripLabel1.Text = "Hazir";
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.Filter = "HDR files|*.hdr";
+            openFileDialog1.FilterIndex = 2;
+            openFileDialog1.RestoreDirectory = true;
+            int i = 0;
+            int index = 0;
+            byte[] pDatas = null;
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    pDatas = ReadAllBytes(openFileDialog1.FileName);
+                    int toplamKayit = BitConverter.ToInt32(pDatas, index); index += 4;
+                    while (toplamKayit > i)
+                    {
+                        int dosyaIsimUzunluk = BitConverter.ToInt32(pDatas, index); index += 4;
+                        byte[] dosya = new byte[dosyaIsimUzunluk];
+                        Array.Copy(pDatas, index, dosya, 0, dosyaIsimUzunluk);
+                        string dosyaAdi = Encoding.UTF8.GetString(dosya, 0, dosyaIsimUzunluk); index += dosyaIsimUzunluk;
+                        int adres = BitConverter.ToInt32(pDatas, index); index += 4;
+                        int boyut = BitConverter.ToInt32(pDatas, index); index += 4;
+                        string adr = String.Format("{0:X}", adres);
+                        string tmp = "0x" +  string.Concat(Enumerable.Repeat("0", 8 - adr.Length)) + adr;
+                        i++;
+                        Cikart(Path.GetDirectoryName(openFileDialog1.FileName),openFileDialog1.SafeFileName, dosyaAdi, adres,boyut);
+                        itemEkle(Convert.ToString(i),  tmp, Convert.ToString(boyut) + " bytes", dosyaAdi);
+                    }
+                    toolStripLabel1.Text = Convert.ToString(toplamKayit) + " tane dosya cikartildi";
+                    MessageBox.Show("Cikartma İslemi Tamamlandi");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("pDatas.Length : {0}, index : {1}", pDatas.Length, index);
+                    MessageBox.Show("Error: Could not read file from disk. Original error: " + ex.Message);
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            listView1.View = View.Details;
+            listView1.Columns.Add("#", 50);
+            listView1.Columns.Add("Adres",75);
+            listView1.Columns.Add("Dosya Boyutu", 85);
+            listView1.Columns.Add("Dosya Adı", 260);
+        }
+        private void itemEkle(string sira,string adres,string boyut,string dosyaAdi)
+        {
+            string[] items = {sira,adres,boyut,dosyaAdi };
+            ListViewItem itm = new ListViewItem(items);
+            listView1.Items.Add(itm);
+
+        }
+    }
+}
